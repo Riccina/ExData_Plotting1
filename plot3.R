b7ac1aca926537931498476d212564e9bb609315
Riccina <msdaelia@gmail.com>
2014-10-14 21:37:53 +1100

    Correcting label for Plot 3

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 ## Getting full dataset
-df <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+df <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 df$Date <- as.Date(df$Date, format="%d/%m/%Y")
 
@@ -15,11 +15,11 @@ data$Datetime <- as.POSIXct(datetime)
 ## Plot 3
 with(data, {
         plot(Sub_metering_1~Datetime, type="l",
-             ylab="Global Active Power (kilowatts)", xlab="")
+             ylab="Energy Sub Metering", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+legend("topright", col=c("black", "red", "blue"), pch = c("-", "-", "-"), lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Saving to file
