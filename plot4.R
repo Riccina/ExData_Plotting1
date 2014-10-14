b18f15c48d86ceb504a2d6b1827ee68659571939
Riccina <msdaelia@gmail.com>
2014-10-14 22:06:36 +1100

    Amended code for Plot4.png
    Amended label for Plot 3 and spacing of legend. Amended label for Plot
    4.

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 ## Getting full dataset
-df <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+df <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 df$Date <- as.Date(df$Date, format="%d/%m/%Y")
 
@@ -12,20 +12,20 @@ datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 4
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+par(mfrow=c(2,2)), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
         plot(Global_active_power~Datetime, type="l", 
              ylab="Global Active Power (kilowatts)", xlab="")
         plot(Voltage~Datetime, type="l", 
              ylab="Voltage (volt)", xlab="")
         plot(Sub_metering_1~Datetime, type="l", 
-             ylab="Global Active Power (kilowatts)", xlab="")
+             ylab="Energy Sub Metering", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
-        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+        legend("topright", col=c("black", "red", "blue"), lwd=2, bty="n", cex=0.3,
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Global_reactive_power~Datetime, type="l", 
-             ylab="Global Rective Power (kilowatts)",xlab="")
+             ylab="Global Reactive Power (kilowatts)",xlab="")
 })
 
 ## Saving to file
